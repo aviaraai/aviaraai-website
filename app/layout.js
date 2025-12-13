e9f14b1e33566072d9769f98f2f844b148778d6c
aacode7 <shaikasifumeed15@gmail.com>
2025-12-13 12:10:53 +0530

    some changes

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,6 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import ContactForm from '@/components/ContactForm'
-
-const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
-  preload: true,
-})
 
 export const metadata = {
   title: 'AviaraAI',
@@ -24,12 +16,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${inter.className} dark`} suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
-      </head>
+    <html lang="en" className="dark" suppressHydrationWarning>
       <body className="bg-light-bg dark:bg-dark-bg text-light-primary dark:text-dark-primary">
         <Navbar />
         <main>{children}</main>
